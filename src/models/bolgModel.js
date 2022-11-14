9ec598a1e70d39d67d05bf7c18d07678df020715
sarwjeet424 <sarwjeet424@gmail.com>
2022-11-14 13:40:47 +0530

    changes

---
+++ src/models/bolgModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+let objectid = mongoose.Schema.Types.ObjectId;
+
+const blogModel = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+
+    body: { type: String, required: true },
+
+    authorId: { type: objectid, ref: Author, require: true },
+
+    tags: [String],
+
+    category: { type: String, required: true },
+
+    subcategory: [String],
+
+    deletedAt: { type: String },
+
+    isDeleted: { type: Boolean, default: false },
+
+    publishedAt: { type: String },
+
+    isPublished: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", blogModel);
